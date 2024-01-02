6df61b889fadd8002d2ffdd4e5ae89f82f94a5a3
geronimo morais <geronimomorais1617@gmail.com>
2024-01-02 19:37:07 -0300

    Validação concluída

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ with open("archive.txt", 'r', encoding="utf-8") as arquivo:
         dados_sem_colchetes = [linha for linha in valor if all('[' not in item and ']' not in item for item in linha)]
         for i ,v in enumerate(dados_sem_colchetes):
             if '+' in dados_sem_colchetes[i]:
-                v= v.replace('kg','').replace('+',' ').split(' ')
+                v= v.replace('kg','').replace('kl', '').replace('+',' ').split(' ')
                 lista_numeros = []
                 for valor in v:
                     if valor.isnumeric():
@@ -29,31 +29,42 @@ with open("archive.txt", 'r', encoding="utf-8") as arquivo:
 
 
             else:
-                if "Dia" not in v:
-                    v = v.replace('kg','').split(' ')
-                    lista_produto_minimo = []
-                    for valor2 in v:
-                        if valor2 == '':
-                            pass
-                        else:
-                            if valor2.isalpha() or valor2 != 'e':
-                                lista_produto_minimo.append(valor2)
-                            else:
+                v = v.replace('kg', '').replace('kl','').lower()
+                if 'dia' not in v:
+                    if 'boa noite' not in v:
+                        v = v.replace('kg','').split(' ')
+                        lista_produto_minimo = []
+                        for valor2 in v:
+                            if valor2 == '':
                                 pass
-                            if valor2.isnumeric():
-                                if valor2 == '1':
-                                    valor2 = 2
-                                    lista_quantidade.append(valor2)
+                            else:
+                                if valor2.isnumeric():
+                                    if valor2 == '1':
+                                        valor2 = 2
+                                        lista_quantidade.append(valor2)
+                                    else:
+                                        valor2 = int(valor2)
+                                        lista_quantidade.append(valor2)
                                 else:
-                                    lista_quantidade.append(int(valor2))
-                    if len(lista_produto_minimo) > 1:
-                        lista_produtos.append(f"{lista_produto_minimo[0]} {lista_produto_minimo[1]}")
-                    else:
-                        lista_produtos.append(*lista_produto_minimo)
-                    lista_datas.append(data_string)
+                                    if valor2.isalpha():
+                                        if valor2 == 'e':
+                                            lista_produtos.append(*lista_produto_minimo)
+                                            lista_datas.append(data_string)
+                                            lista_produto_minimo.clear()
+                                        else:
+                                            lista_produto_minimo.append(valor2)
+
+                                    else:
+                                        pass
+
+                        if len(lista_produto_minimo) > 1:
+                            lista_produtos.append(f"{lista_produto_minimo[0]} {lista_produto_minimo[1]}")
+                        else:
+                            lista_produtos.append(*lista_produto_minimo)
+                        lista_datas.append(data_string)
 
                 else:
-                    if "Dia" in v:
+                    if "dia" in v:
                         data_atual = date.today()
                         data_string = data_atual.strftime("%d-%m-%Y")
 
