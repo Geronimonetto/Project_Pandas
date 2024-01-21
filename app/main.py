f0012f104f261ff21faecdd2b94065de1b97b4df
geronimo morais <geronimomorais1617@gmail.com>
2024-01-21 16:22:57 -0300

    Projeto Pandas - Enviando atualização de projeto

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
-from datetime import date
+from datetime import date, timedelta
 
-with open("../alo.txt", 'r', encoding="utf-8") as arquivo:
+with open("D:/Github/03.Projetc_Pandas/archive.txt", 'r', encoding="utf-8") as arquivo:
     lista_frutas = ['Melancia', 'Melão', 'Melão Português', 'Mamão Avai', 'Mamão Formosa', 'Melão Japonês', 'Jerimum']
     lista_datas = []
     lista_produtos = []
@@ -12,8 +12,22 @@ with open("../alo.txt", 'r', encoding="utf-8") as arquivo:
         # Exclua os elementos que contêm "[" ou "]"
         dados_sem_colchetes = [linha for linha in valor if all('[' not in item and ']' not in item for item in linha)]
         for i ,v in enumerate(dados_sem_colchetes):
+            v = v.strip()
             data_atual = date.today()
-            data_string = data_atual.strftime("%d-%m-%Y")
+            if v == 'Dia 1':
+                data_espc = data_atual - timedelta(5)
+            elif v == 'Dia 2':
+                data_espc = data_atual - timedelta(4)
+            elif v == 'Dia 3':
+                data_espc = data_atual - timedelta(3)
+            elif v == 'Dia 4':
+                data_espc = data_atual - timedelta(2)
+            elif v == 'Dia 5':
+                data_espc = data_atual - timedelta(1)
+            elif v == 'Dia 6':
+                data_espc = data_atual
+
+
             if '+' in dados_sem_colchetes[i]:
 
                 v= v.replace('kg','').replace('kl', '').replace('+',' ').split(' ')
@@ -29,7 +43,7 @@ with open("../alo.txt", 'r', encoding="utf-8") as arquivo:
                             lista_produtos.append(valor)
                 lista_quantidade.append(sum(lista_numeros))
                 lista_numeros.clear()
-                lista_datas.append(data_string)
+                lista_datas.append(data_espc)
 
 
             else:
@@ -39,7 +53,7 @@ with open("../alo.txt", 'r', encoding="utf-8") as arquivo:
                         v = v.replace('kg','').split(' ')
                         lista_produto_minimo = []
                         for valor2 in v:
-                            if valor2 == '':
+                            if valor2 == '' or valor2 == 'und' or valor2 == 'm':
                                 pass
                             else:
                                 if valor2.isnumeric():
@@ -52,41 +66,47 @@ with open("../alo.txt", 'r', encoding="utf-8") as arquivo:
                                 else:
                                     if valor2.isalpha():
                                         valor2 = valor2.capitalize()
-                                        if valor2 == 'e':
+                                        if valor2 == 'E':
                                             lista_produtos.append(*lista_produto_minimo)
-                                            lista_datas.append(data_string)
+                                            lista_datas.append(data_espc)
                                             lista_produto_minimo.clear()
                                         else:
                                             if valor2 in lista_frutas:
                                                 if valor2 == 'Melão' or valor2 == 'Melao':
-                                                    valor2 = 'Melão Espanhol'
-                                                    lista_produto_minimo.append(valor2.capitalize())
+                                                    valor2 = 'Melão espanhol'
+                                                    lista_produto_minimo.append(valor2)
                                                 else:
-                                                    lista_produto_minimo.append(valor2.capitalize())
+                                                    lista_produto_minimo.append(valor2)
                                             else:
                                                 if valor2 == 'Formosa' and len(lista_produto_minimo) ==0:
-                                                    valor2 = 'Mamão Formosa'
-                                                    lista_produto_minimo.append(valor2.capitalize())
+                                                    valor2 = 'Mamão formosa'
+                                                    lista_produto_minimo.append(valor2)
                                                 else:
                                                     if valor2 == 'Melào':
                                                         valor2 = 'Melão'
-                                                        lista_produto_minimo.append(valor2.capitalize())
+                                                        lista_produto_minimo.append(valor2)
                                                     elif valor2 == 'Espanho':
-                                                        valor2 = 'Espanhol'
-                                                        lista_produto_minimo.append(valor2.capitalize())
+                                                        valor2 = 'espanhol'
+                                                        lista_produto_minimo.append(valor2)
+                                                    elif valor2 =='Jirimum':
+                                                        valor2 = 'Jerimum'
+                                                        lista_produto_minimo.append(valor2)
                                                     else:
-                                                        lista_produto_minimo.append(valor2.capitalize())
+                                                        lista_produto_minimo.append(valor2)
                                     else:
                                         pass
 
                         if len(lista_produto_minimo) > 1:
                             lista_produtos.append(f"{lista_produto_minimo[0]} {lista_produto_minimo[1].lower()}")
                         else:
-                            lista_produtos.append(*lista_produto_minimo)
-                        lista_datas.append(data_string)
+                            if lista_produto_minimo[0] == 'Melão':
+                                lista_produtos.append('Melão espanhol')
+                            else:
+                                lista_produtos.append(*lista_produto_minimo)
+                        lista_datas.append(data_espc)
 
 
     dados = {'Produtos': lista_produtos, 'Quantidade': lista_quantidade, 'Data': lista_datas}
     df = pd.DataFrame(dados)
-    df.to_excel('arquivo2.xlsx')
+    df.to_excel('D:/Github/03.Projetc_Pandas/archive.xlsx')
     print(df)
